d465ee4a81f759c6c106cfe5bb9aea639ed3a96a
Will Shen <muyousome@gmail.com>
2016-03-05 22:25:47 +0800

    新增数据流注释

--- src/IO/TestDataStream.java
+++ src/IO/TestDataStream.java
@@ -24,21 +24,37 @@ import java.io.*;
 class TestDataStream{
 	public static void main(String[] args) {
 		try{
+
+			//新建字节数组且关联输出流
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+			//新建数据流，嵌套在字节数组流上
 			DataOutputStream dos = new DataOutputStream(baos);
+
+			//通过数据流向字节数组写入数据
 			dos.writeDouble(Math.random());
 			dos.writeBoolean(false);
 
+			//新建字节数组输入流，关联已存在的字节数组
 			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+
+			//新建数据输入流，嵌套在字节数组输入流上
 			DataInputStream dis = new DataInputStream(bais);
+
+
+			//可读取的数据
 			System.out.println(bais.available());
+			
+			//从字节数组读取数据且通过数据流转换
 			System.out.println(dis.readDouble());
 			System.out.println(dis.readBoolean());
 
+			//释放资源
 			baos.close();
 			dos.close();
 			bais.close();
 			dis.close();
+
 		}catch(IOException e){
 			e.printStackTrace();
 		}
